46b7f6a9cec5024c6c820d82bbb3eebdd541bb73
tmedina23 <tmedina23@bsmschool.org>
2023-03-02 15:55:04 -0600

    commmti

--- auto.py
+++ auto.py
@@ -1,4 +1,15 @@
 import subprocess
 
 output = subprocess.check_output(['v4l2-ctl' , '--list-devices'])
+
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub)
+
+list(find_all(output, "\n"))
+
 print(output)
