e2570dede1aa39869b470784a5d33ba283c3e418
tmedina23 <tmedina23@bsmschool.org>
2023-03-02 16:13:55 -0600

    vfdjfd

--- auto.py
+++ auto.py
@@ -5,15 +5,15 @@ output = subprocess.check_output(['v4l2-ctl' , '--list-devices'])
 def find_all(a_str, sub):
     start = 0
     while True:
-        start = a_str.find(sub, start)
+        start = a_str.find(sub, start,)
         if start == -1: return
         yield start
         start += len(sub)
 
-instances = list(find_all(str(output), "/dev/video", 10))
+instances = list(find_all(str(output), "\n"))
 
 for x in range(len(instances)):
-    substring = output[instances[x]-1:instances[x]+7]
+    substring = output[instances[x]:instances[x]+1]
     print(substring)
 
 print(output)
