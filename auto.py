7c562d058e122520b98ef934887758830b805fd6
tmedina23 <tmedina23@bsmschool.org>
2023-03-13 14:46:34 -0500

    add

--- auto.py
+++ auto.py
@@ -16,7 +16,7 @@ final = [999,999]
 instance = str(output).find(searching_for1)
 
 def init_vars(output):
-    name = output[instance:instance+len(searching_for1)]
+    name = output[instance-len(searching_for1):instance]
     cams.append(name)
     index = output[instance+len(searching_for1)+38:instance+len(searching_for1)+39]
     indices.append(index)
