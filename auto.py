7c39f802cf39514c4f13ac54e86464ed0c333ecd
tmedina23 <tmedina23@bsmschool.org>
2023-03-14 11:01:39 -0500

    new split method

--- auto.py
+++ auto.py
@@ -7,20 +7,27 @@ import cv2
 import numpy as np
 from pysondb import db
 
-output = subprocess.check_output(['v4l2-ctl', '--list-devices'])
+output = str(subprocess.check_output(['v4l2-ctl', '--list-devices']))
 searching_for1 = "USB 2.0 Camera: HD USB Camera"
 cams = []
 indices = []
 final = [999,999]
 
-instance = str(output).find(searching_for1)
+instance = output.find(searching_for1)
 
-def init_vars(output):
+def init_vars():
     name = output[instance:instance+len(searching_for1)]
     cams.append(name)
-    print(output)
     index = output[instance+len(searching_for1)+38:instance+len(searching_for1)+39]
     indices.append(index)
+
+def init_vars_split():
+    split_newline = output.split('\n\n')
+    for x in split_newline:
+        name = x.split(" (")[0]
+        index = x.split("video")[1]
+        cams.append(name)
+        indices.append(index)
 
 def assign():
     for l in range(len(cams)):
@@ -36,9 +43,21 @@ def assign():
 
     return final
 
-print(str(output))
+print("Testing Og way:")
 print("\n")
-init_vars(output)
+print(output)
+init_vars()
+print("\n")
+print("Name: " + str(instance) +" "+ str(len(searching_for1)))
+print(*cams, sep = ", ")
+print("\n")
+print("Index: " + str(instance+38))
+print(*indices, sep = ", ")
+
+print("Testing new way:")
+print("\n")
+print(output)
+init_vars_split()
 print("\n")
 print("Name: " + str(instance) +" "+ str(len(searching_for1)))
 print(*cams, sep = ", ")
