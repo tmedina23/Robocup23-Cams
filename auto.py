0be369c77cf16ecbca337411fb9351b30e070e96
tmedina23 <tmedina23@bsmschool.org>
2023-03-16 10:24:48 -0500

    dfvgohjdfksgda

--- auto.py
+++ auto.py
@@ -70,6 +70,7 @@ def getindexdb(id, ret_string):
 
 #check for changes in device_number using "lsusb"
 def checkUpdate():
+    print(output_ls)
     presliced = output_ls.split(":")[0]
     dev_number = int(presliced[-2:])
     if(dev_number > getindexdb(283699290575417516,False)):
@@ -84,3 +85,5 @@ def run_auto():
     print("updating database...")
     updateDB()
     print("complete")
+
+checkUpdate()
