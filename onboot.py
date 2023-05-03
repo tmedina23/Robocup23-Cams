c7b7da5e9a395fdeeaf22387942a6c33ae89fa02
tmedina23 <tmedina23@bsmschool.org>
2023-05-03 12:20:40 -0500

    ok

--- onboot.py
+++ onboot.py
@@ -1,8 +1,10 @@
 import auto
+import time
 from pysondb import db
 
 camdb = db.getDb("camdb.json")
 
+time.sleep(5)
 auto.run_auto()
 camdb.updateById(209847509711096578,{"index":"False"})
 camdb.updateById(283699290575417516,{"index":"7"})
