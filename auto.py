9b24fbd44798667ae5e4ef1f2a89a47183414bb3
Thomas Medina <71028513+tmedina23@users.noreply.github.com>
2023-06-19 11:17:19 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -8,10 +8,12 @@ import numpy as np
 from pysondb import db
 
 #Runs the command and saves the output
+#if you don't know what happens when you run these commands, please try them on your own.
 output = str(subprocess.check_output(['v4l2-ctl', '--list-devices']))
 output_ls = str(subprocess.check_output(['lsusb']))
 output_ip = str(subprocess.check_output(['ifconfig']))
-#System names for each camera device
+#System names for each of the camera devices we are looking for
+#There are only 3 names instead of four because 2 cameras will have the same name
 searching = {"USB 2.0 Camera: HD USB Camera", "HD USB Camera: HD USB Camera", "HD USB Camera: USB Camera"}
 #names of each camera get saved here
 cams = []
@@ -22,24 +24,37 @@ indices = []
 camdb=db.getDb("camdb.json")
 
 #helper function
+#Splices the 'output' to find the name of the device and the index.
 def name_index(instance, device):
+    #instance is the index of the device name, .find is passed as an argument
     name = output[instance:instance+len(device)]
+    #add that to the end of the cams array
     cams.append(name)
+    #finds the index of the using the device name
+    #index is always 38 characters after the end of the device name
     index = output[instance+len(device)+38:instance+len(device)+40]
+    #if the index is one character
     if("\\" in index):
         index = output[instance+len(device)+38:instance+len(device)+39]
         indices.append(index)
+    #if the index is two characters
     else:
         indices.append(index)
 
 #Sets the arrays using data from the subprocess command
+#this is where the name_index function gets used
 def set_arrays():
+    #for each of the 3 device names
     for device in searching:
+        #the index of the device name
         instance = output.find(device)
+        #if the name is the duplicate name
         if (device == "HD USB Camera: USB Camera"):
+            #finds the index twice
             name_index(instance, device)
             name_index(output.find(device, instance+len(device)+1), device)
         else:
+            #just once
             name_index(instance,device)
 
 #Updates the database using database ids and arrays
@@ -59,6 +74,7 @@ def updateDB():
             camdb.updateById("870675630757077322",{"index":str(indices[l])})
 
 #function not used in this file, helper function for app.py
+#pass the second argument as True to pass the index as a string
 def getindexdb(id, ret_string):
     camdata = str(camdb.getById(id))
     #{"name": "final","pos": "final","index": "False","id": 209847509711096578}
@@ -75,19 +91,27 @@ def getindexdb(id, ret_string):
 
 #check for changes in device_number using "lsusb"
 def checkUpdate():
+    #split by row
     all_devices = output_ls.split("\\n")
+    #highest device number
     highest = 0
+    #for each line in the output
     for x in range(len(all_devices)-1):
+        #splice to find the device number
         presliced = all_devices[x].split(":")[0]
         dev_number = int(presliced[-2:])
+        #saves the highest device number by the end of the loop
         if(dev_number > highest):
             highest = dev_number
     print("Current Highest Device Number: " + str(highest))
+    #if the most recent highest device number is greater than the one saved in the database
     if(highest > getindexdb(283699290575417516, False)):
+        #update the db
         camdb.updateById(209847509711096578,{"index":"False"})
         camdb.updateById(283699290575417516,{"index":highest})
         print("Database Updated: New highest dev_number")
     else:
+        #sets the final value to true
         camdb.updateById(209847509711096578,{"index":"True"})
 
 def ip():
